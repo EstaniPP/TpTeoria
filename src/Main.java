5d4e33160e592d69d2028452dd9d423657d40693
estani <estanipp@gmail.com>
2019-04-28 16:20:58 -0300

    histograma prueba andando

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 import java.awt.Color;
 import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 import java.util.logging.Logger;
 
+import java.io.*;
+import java.util.Random;
+import org.jfree.chart.*;
+import org.jfree.data.statistics.*;
+import org.jfree.chart.plot.PlotOrientation;
 
 public class Main {
 	
@@ -27,4 +34,30 @@ public class Main {
 			System.out.println(i+": "+suma[i]);
 	}
 	
+	
+		double[] value = new double[100];
+		Random generator = new Random();
+		for (int i=0; i < 100; i++) {
+		   value[i] = generator.nextDouble();
+		}
+	    int number = 10;
+	    HistogramDataset dataset = new HistogramDataset();
+	    dataset.setType(HistogramType.RELATIVE_FREQUENCY);
+	    dataset.addSeries("Histogram",value,number);
+	    String plotTitle = "Histogram"; 
+	    String xaxis = "number";
+	    String yaxis = "value"; 
+	    PlotOrientation orientation = PlotOrientation.VERTICAL; 
+	    boolean show = false; 
+	    boolean toolTips = false;
+	    boolean urls = false; 
+	    JFreeChart chart = ChartFactory.createHistogram( plotTitle, xaxis, yaxis, dataset, orientation, show, toolTips, urls);
+	    
+	    int width = 500;
+	    int height = 300; 
+	    
+	    try {
+	 	   ChartUtilities.saveChartAsPNG(new File("histogram.PNG"), chart, width, height);
+	    } catch (IOException e) {}
+	}
 }
