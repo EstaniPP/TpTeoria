3369e6707f1376eb49b2f322d8d00feeea12c45a
Valentin <valentinfvazquez@gmail.com>
2019-05-01 00:26:21 -0300

    FileChooser added

--- src/gui.java
+++ src/gui.java
@@ -1,4 +1,9 @@
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -7,10 +12,18 @@ import javax.swing.JDesktopPane;
 import javax.swing.JFileChooser;
 
 import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class gui {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -35,15 +48,57 @@ public class gui {
 		initialize();
 	}
 
+	public static BufferedImage resize(BufferedImage img, int newW, int newH) { 
+	    Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+	    BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+	    Graphics2D g2d = dimg.createGraphics();
+	    g2d.drawImage(tmp, 0, 0, null);
+	    g2d.dispose();
+
+	    return dimg;
+	} 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		final JFileChooser fc = new JFileChooser();
-		int val = fc.showOpenDialog(null);
+		lblNewLabel = new JLabel("");
+		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 696, 383);
+		frame.setBounds(100, 100, 415, 501);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		JLabel lblSeleccioneImagen = new JLabel("Seleccione imagen");
+		lblSeleccioneImagen.setBounds(6, 6, 137, 16);
+		frame.getContentPane().add(lblSeleccioneImagen);
+		
+		textField = new JTextField();
+		textField.setBounds(3, 26, 295, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnSeleccionar = new JButton("SELECCIONAR");
+		btnSeleccionar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+				int seleccion = fileChooser.showOpenDialog(gui.this.lblNewLabel);
+				if(seleccion == JFileChooser.APPROVE_OPTION) {
+					File image = fileChooser.getSelectedFile();
+					ImageParser p = new ImageParser(image);
+					gui.this.textField.setText(image.getPath());
+					gui.this.lblNewLabel.setIcon(new ImageIcon(gui.this.resize(p.getBI(), 403, 383)));
+				}
+			}
+		});
+		btnSeleccionar.setBounds(299, 26, 117, 29);
+		frame.getContentPane().add(btnSeleccionar);
+		
+		
+		lblNewLabel.setBounds(6, 59, 403, 383);
+		frame.getContentPane().add(lblNewLabel);
+		
+		
 	}
 }
