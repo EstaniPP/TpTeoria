df3946f34aabe5fa0f7b243d1b0dc5195291ee47
Valentin <valentinfvazquez@gmail.com>
2019-05-01 14:41:53 -0300

    Deleted

--- src/Utilities.java
+++ src/Utilities.java
@@ -1,4 +1,12 @@
 import java.io.File;
+import java.io.IOException;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.data.statistics.HistogramType;
 
 public class Utilities {
 	
@@ -66,5 +74,36 @@ public class Utilities {
 		}
 		
 		return matrizcond;
+	}
+	
+	public static void createHistogram(ImageParser img, String chartName) {
+		
+		double[] bufferImagen = new double[250000];
+		ImageParser bloque = img.getBlock(0, 0);
+		for(int j=0;j<500;j++) {
+			for(int i=0;i<500;i++) {
+				bufferImagen[j*500+i] = (double) bloque.getRGB(i, j).getRed();
+			}
+		}
+	    HistogramDataset dataset = new HistogramDataset();
+	    dataset.setType(HistogramType.FREQUENCY);
+	    dataset.addSeries("Histograma", bufferImagen, 256);
+	    String plotTitle = "Histograma de entropias"; 
+	    String xaxis = "Entropias";
+	    String yaxis = "Valor"; 
+	    PlotOrientation orientation = PlotOrientation.VERTICAL; 
+	    boolean show = false; 
+	    boolean toolTips = false;
+	    boolean urls = false; 
+	    JFreeChart chart = ChartFactory.createHistogram( plotTitle, xaxis, yaxis, dataset, orientation, show, toolTips, urls);
+	    
+	    int width = 500;
+	    int height = 300; 
+	    
+	    try {
+	    	ChartUtilities.saveChartAsPNG(new File(chartName), chart, width, height);
+	    } catch (IOException e) {
+	    	
+	    }
 	}
 }
